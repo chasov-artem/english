7c33b71161608c22781986b76168fc72e7e63031
Your Name <chasov90@gmail.com>
2025-10-22 13:12:28 +0300

    styles: home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
+import headImage from "../../assets/head.png";
+import headImage2x from "../../assets/head_2x.png";
+import macImage from "../../assets/mac.png";
+import macImage2x from "../../assets/mac_2x.png";
 
 const Home = () => {
   return (
@@ -25,9 +29,18 @@ const Home = () => {
             </Link>
           </div>
           <div className={styles.heroRight}>
-            <div className={styles.heroImage}>
-              <div className={styles.character}>👨‍💻</div>
-            </div>
+            <img
+              src={headImage}
+              srcSet={`${headImage} 1x, ${headImage2x} 2x`}
+              alt="Head"
+              className={styles.head}
+            />
+            <img
+              src={macImage}
+              srcSet={`${macImage} 1x, ${macImage2x} 2x`}
+              alt="Mac"
+              className={styles.mac}
+            />
           </div>
         </div>
       </section>
@@ -36,19 +49,27 @@ const Home = () => {
         <div className={styles.statsContainer}>
           <div className={styles.statItem}>
             <div className={styles.statNumber}>32,000 +</div>
-            <div className={styles.statLabel}>Experienced tutors</div>
+            <div className={styles.statLabel}>
+              Experienced<br></br> tutors
+            </div>
           </div>
           <div className={styles.statItem}>
             <div className={styles.statNumber}>300,000 +</div>
-            <div className={styles.statLabel}>5-star tutor reviews</div>
+            <div className={styles.statLabel}>
+              5-star tutor<br></br> reviews
+            </div>
           </div>
           <div className={styles.statItem}>
             <div className={styles.statNumber}>120 +</div>
-            <div className={styles.statLabel}>Subjects taught</div>
+            <div className={styles.statLabel}>
+              Subjects<br></br> taught
+            </div>
           </div>
           <div className={styles.statItem}>
             <div className={styles.statNumber}>200 +</div>
-            <div className={styles.statLabel}>Tutor nationalities</div>
+            <div className={styles.statLabel}>
+              Tutor<br></br> nationalities
+            </div>
           </div>
         </div>
       </section>
